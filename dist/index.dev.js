7983794a79be5c0197ea1e3a3f9b661546b6ee07
squarerobin <luis.abellan.dev@gmail.com>
2020-04-08 06:27:15 +0200

    set up- starting challenge

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,36 +1,32 @@
+"use strict";
+
 // implement your API here
 // require the express npm module, needs to be added to the project using "npm install express"
-const express = require('express');
-const db = require('./data/db.js')
+var express = require('express');
 
-const port = 8000
+var db = require('./data/db.js');
 
-// creates an express application using the express module
-const server = express();
+var port = 8000; // creates an express application using the express module
 
-// configures our server to execute a function for every GET request to "/"
+var server = express(); // configures our server to execute a function for every GET request to "/"
 // the second argument passed to the .get() method is the "Route Handler Function"
 // the route handler function will run on every GET request to "/"
-server.get('/', (req, res) => {
+
+server.get('/', function (req, res) {
   // express will pass the request and response objects to this function
   // the .send() on the response object can be used to send a response to the client
   res.send('Hello World');
 });
-
-server.get("/hobbits", (req, res) => {
+server.get("/hobbits", function (req, res) {
   // route handler code here
-  const hobbits = [
-    {
-      id: 1,
-      name: "Samwise Gamgee"
-    },
-    {
-      id: 2,
-      name: "Frodo Baggins"
-    }
-  ];
-  
-   res.status(200).json(hobbits);
+  var hobbits = [{
+    id: 1,
+    name: "Samwise Gamgee"
+  }, {
+    id: 2,
+    name: "Frodo Baggins"
+  }];
+  res.status(200).json(hobbits);
 });
 /* Challenge
 Write an endpoint that returns a list of users stored in 
@@ -51,17 +47,14 @@ so make sure to send the response after that promise
 has resolved and, in case of failure, return a status 
 code of 500 and an error message back to the client. */
 
-server.get("/users", (req, res) => {
+server.get("/users", function (req, res) {
   // route handler code here
-  const users = db.find()
-   
-  //res.send(users);
-   
+  var users = db.find(); //res.send(users);
 
   res.status(200).json(users);
-});
-
-
-// once the server is fully configured we can have it "listen" for connections on a particular "port"
+}); // once the server is fully configured we can have it "listen" for connections on a particular "port"
 // the callback function passed as the second argument will run once when the server starts
-server.listen(port, () => console.log(`API running on port ${port}`));
+
+server.listen(port, function () {
+  return console.log("API running on port ".concat(port));
+});
